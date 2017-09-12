7d1e9ce6a5d7f465425d9a8d25c4f8408d95dd88
Binh Quan <binhqd@gmail.com>
2017-09-12 11:21:03 +0700

    Update Login layout

--- components/layouts/login/Login.jsx
+++ components/layouts/login/Login.jsx
@@ -13,28 +13,7 @@ class LoginLayout extends React.Component {
         <div className="login-box-body">
           <p className="login-box-msg">Sign in to start your session</p>
 
-          <form action="../../index2.html" method="post">
-            <div className="form-group has-feedback">
-              <input type="email" className="form-control" placeholder="Email"/>
-              <span className="glyphicon glyphicon-envelope form-control-feedback"></span>
-            </div>
-            <div className="form-group has-feedback">
-              <input type="password" className="form-control" placeholder="Password"/>
-              <span className="glyphicon glyphicon-lock form-control-feedback"></span>
-            </div>
-            <div className="row">
-              <div className="col-xs-8">
-                <div className="checkbox icheck">
-                  <label>
-                    <input type="checkbox" /> Remember Me
-                  </label>
-                </div>
-              </div>
-              <div className="col-xs-4">
-                <button type="submit" className="btn btn-primary btn-block btn-flat">Sign In</button>
-              </div>
-            </div>
-          </form>
+          {this.props.children}
 
           <div className="social-auth-links text-center">
             <p>- OR -</p>
--- components/pages/Login.jsx
+++ components/pages/Login.jsx
@@ -5,9 +5,28 @@ class Login extends React.Component {
   render() {
     return (
       <LoginLayout>
-        <div>
-          This is login page
-        </div>
+        <form action="../../index2.html" method="post">
+          <div className="form-group has-feedback">
+            <input type="email" className="form-control" placeholder="Email"/>
+            <span className="glyphicon glyphicon-envelope form-control-feedback"></span>
+          </div>
+          <div className="form-group has-feedback">
+            <input type="password" className="form-control" placeholder="Password"/>
+            <span className="glyphicon glyphicon-lock form-control-feedback"></span>
+          </div>
+          <div className="row">
+            <div className="col-xs-8">
+              <div className="checkbox icheck">
+                <label>
+                  <input type="checkbox" /> Remember Me
+                </label>
+              </div>
+            </div>
+            <div className="col-xs-4">
+              <button type="submit" className="btn btn-primary btn-block btn-flat">Sign In</button>
+            </div>
+          </div>
+        </form>
       </LoginLayout>
     );
   }
