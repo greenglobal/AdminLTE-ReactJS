8259b3e65af378e4d33147965947a56deebf4e45
Binh Quan <binhqd@gmail.com>
2017-09-22 16:11:16 +0700

    Remove console.log in MenuItem component

--- components/UI/Menu/MenuItem.jsx
+++ components/UI/Menu/MenuItem.jsx
@@ -3,7 +3,6 @@ import {PropTypes} from 'prop-types';
 import {NavLink, withRouter} from 'react-router-dom';
 
 let MenuItem = (props) => {
-  console.log(props);
   return (
     <li className={props.match.path == props.to ? props.activeClassName : ''}>
       <NavLink to={props.to} activeClassName={props.activeClassName}><i className={props.className || 'fa fa-circle-o'}></i>{props.label}
